fdd9550adcc9f30604a25668938695f2c98e1cda
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2026-09-14 16:59:51 +0200

    app-provider changed, already clean up from next-intl

--- src/components/providers/app-providers.tsx
+++ src/components/providers/app-providers.tsx
@@ -1,24 +1,17 @@
 "use client";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import { NextIntlClientProvider, AbstractIntlMessages } from "next-intl";
+
 
 interface AppProvidersProps {
   children: React.ReactNode;
   locale: string;
-  messages: AbstractIntlMessages;
 }
 
-export function AppProviders({ children, locale, messages }: AppProvidersProps) {
+export function AppProviders({ children }: AppProvidersProps) {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="theme">
-      <NextIntlClientProvider 
-        locale={locale} 
-        messages={messages}
-        timeZone="UTC"
-      >
         {children}
-      </NextIntlClientProvider>
     </ThemeProvider>
   );
 }
